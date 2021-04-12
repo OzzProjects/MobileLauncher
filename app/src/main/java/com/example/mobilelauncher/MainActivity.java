7c748b5e1e99aa2015d5fa21151592cdb3653d42
Osezele <osezele06@gmail.com>
2021-04-12 20:03:31 +0100

    Layout setup - Added single page layout with GridView (view_viewpager.xml). Created empty ViewPagerAdapter.

--- app/src/main/java/com/example/mobilelauncher/MainActivity.java
+++ app/src/main/java/com/example/mobilelauncher/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.mobilelauncher;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
+import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 
 import java.util.ArrayList;
@@ -15,12 +19,15 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG="Logger";
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
 
         Log.i(TAG,"ON CREATE.");
         Intent intent = new Intent(Intent.ACTION_MAIN, null);
@@ -35,6 +42,17 @@ public class MainActivity extends AppCompatActivity {
             Log.i(TAG,"Application: "+appName);
         }
 
+        setupViewPager();
+
+
     }
-    
+
+
+    public void setupViewPager(){
+        viewPager= findViewById(R.id.viewpager);
+        viewPagerAdapter=new ViewPagerAdapter();
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/mobilelauncher/ViewPagerAdapter.java
@@ -0,0 +1,33 @@
+package com.example.mobilelauncher;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        return super.instantiateItem(container, position);
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        super.destroyItem(container, position, object);
+    }
+
+    @Override
+    public int getCount() {
+        return 1;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+      return false;
+    }
+}
