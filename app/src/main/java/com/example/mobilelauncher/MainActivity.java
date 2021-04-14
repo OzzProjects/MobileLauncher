9524f8563f35da9482e3c0a61d1b350e97c51c2c
Osezele <osezele06@gmail.com>
2021-04-14 20:39:32 +0100

    BottomSheet - Added BottomSheetBehavior Layout in collapsed state

--- app/src/main/java/com/example/mobilelauncher/MainActivity.java
+++ app/src/main/java/com/example/mobilelauncher/MainActivity.java
@@ -11,7 +11,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.GridView;
 import android.widget.ImageButton;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,37 +24,48 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG="Logger";
     ViewPager viewPager;
     ViewPagerAdapter viewPagerAdapter;
+    BottomSheetBehavior bottomSheetBehavior;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-
-
-        Log.i(TAG,"ON CREATE.");
-        Intent intent = new Intent(Intent.ACTION_MAIN, null);
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-
-        List<ResolveInfo> appList = getApplicationContext().getPackageManager().queryIntentActivities(intent, 0);
-        Log.i(TAG,"RETRIEVING APPLICATION LIST FROM PACKAGE MANAGER.. ");
-        for(ResolveInfo currentApp : appList) {
-            String appName = currentApp.activityInfo.loadLabel(getPackageManager()).toString();
-            String appPackageName = currentApp.activityInfo.packageName;
-            Drawable appImage = currentApp.activityInfo.loadIcon(getPackageManager());
-            Log.i(TAG,"Application: "+appName);
-        }
-
-        setupViewPager(appList);
-
+        setupViewPager();
+        setupBottomSheet();
 
     }
 
+    public List<ResolveInfo> getBottomAppList(){
+        List<ResolveInfo> collapsedBottomList=getAppList().subList(0,4);
+        return collapsedBottomList;
+    }
 
-    public void setupViewPager(List<ResolveInfo> appList){
+    public List<ResolveInfo> getAppList(){
+        Intent intent = new Intent(Intent.ACTION_MAIN, null);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> appList = getApplicationContext().getPackageManager().queryIntentActivities(intent, 0);
+        return appList;
+    }
+
+    public void setupViewPager(){
         viewPager= findViewById(R.id.viewpager);
+        List<ResolveInfo> appList=getAppList();
         viewPagerAdapter=new ViewPagerAdapter(this,appList);
         viewPager.setAdapter(viewPagerAdapter);
+    }
+
+    public void setupBottomSheet(){
+        final View bottomsheet=findViewById(R.id.bottomsheet);
+        bottomSheetBehavior=BottomSheetBehavior.from(bottomsheet);
+        bottomSheetBehavior.setPeekHeight(400);
+        bottomSheetBehavior.setHideable(false);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+        final GridView bottomGrid=findViewById(R.id.bottomGrid);
+        bottomGrid.setAdapter(new GridAdapter(this,getBottomAppList()));
+
     }
 
 
